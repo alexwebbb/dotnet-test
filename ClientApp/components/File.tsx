aa7f9c8091cc2162ea50de8d260923984b3642dd
Alex Webb <alexwebbb@yahoo.com>
2017-06-24 20:34:54 -0500

    some more hello world-ish stuff

---
+++ ClientApp/components/File.tsx
@@ -0,0 +1,17 @@
+﻿import * as React from 'react';
+import { RouteComponentProps } from 'react-router-dom';
+
+export default class File extends React.Component<RouteComponentProps<{ poop: string }>, {}> {
+    public render() {
+        return <div>
+            <h1>Hello freaking world</h1>
+            <div>
+            <p>Ok, I am adding a bit of text</p>
+            </div>
+            <div>
+                {this.props.match.params.poop}
+            </div>
+        </div>
+            
+    }
+}
--- ClientApp/components/NavMenu.tsx
+++ ClientApp/components/NavMenu.tsx
@@ -32,6 +32,11 @@ export class NavMenu extends React.Component<{}, {}> {
                                 <span className='glyphicon glyphicon-th-list'></span> Fetch data
                             </NavLink>
                         </li>
+                        <li>
+                            <NavLink to={'/file'} activeClassName='active'>
+                                <span className='glyphicon glyphicon-flash'></span> File
+                            </NavLink>
+                        </li>
                     </ul>
                 </div>
             </div>
